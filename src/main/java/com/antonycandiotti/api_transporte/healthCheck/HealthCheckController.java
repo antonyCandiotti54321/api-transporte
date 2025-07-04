228e4635890f8bb0d8b6e18e53d626765dc30f7a
antonyCandiotti54321 <antonycm54321@gmail.com>
2025-07-03 23:15:46 -0500

    Pings por primera vez y luego cada 4 horas

--- src/main/java/com/antonycandiotti/api_transporte/healthCheck/HealthCheckController.java
+++ src/main/java/com/antonycandiotti/api_transporte/healthCheck/HealthCheckController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping
 public class HealthCheckController {
@@ -12,7 +14,14 @@ public class HealthCheckController {
    @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         System.out.println("OK Ping Web");
-        return ResponseEntity.ok("OK");
+       return ResponseEntity.ok("{\"status\": \"ok\", \"time\": \"" + LocalDateTime.now() + "\"}");
+
+   }
+    @GetMapping("api/health")
+    public ResponseEntity<String> healthCheck() {
+
+        System.out.println("OK healthcheck");
+        return ResponseEntity.ok("{\"status\": \"ok\", \"time\": \"" + LocalDateTime.now() + "\"}");
     }
 
 
